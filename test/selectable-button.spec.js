e163d65e7a29567d44f05dfa432f188f3bac6c34
Alex Lanz <alex@krumer.it>
2019-08-29 14:19:17 +0200

    Seletable Button Tests

---
+++ test/selectable-button.spec.js
@@ -0,0 +1,53 @@
+import { mount } from '@vue/test-utils'
+import SelectableButton from '@/components/selectable-button.vue'
+
+describe('selectable-button', () => {
+  test('is not marked as selected', () => {
+    const wrapper = mount(SelectableButton, {
+      propsData: {
+        text: 'Italy',
+        value: 'IT',
+        selectedValue: 'ITX',
+        selectedColor: 'metric0',
+        click: () => {}
+      }
+    })
+    expect(wrapper.html()).toContain('Italy')
+    expect(wrapper.classes()).toContain('border-black')
+  })
+
+  test('is marked as selected', () => {
+    const wrapper = mount(SelectableButton, {
+      propsData: {
+        text: 'Italy',
+        value: 'IT',
+        selectedValue: 'IT',
+        selectedColor: 'metric0',
+        click: () => {}
+      }
+    })
+    expect(wrapper.html()).toContain('Italy')
+    expect(wrapper.classes()).toContain('border-metric0-500')
+  })
+
+  test('fires event when clicked', () => {
+    let selectedValue = 'ITX'
+
+    const wrapper = mount(SelectableButton, {
+      propsData: {
+        text: 'Italy',
+        value: 'IT',
+        selectedValue: selectedValue,
+        selectedColor: 'metric0',
+        click: (value) => {
+          selectedValue = value
+        }
+      }
+    })
+
+    const button = wrapper.find('button')
+    button.trigger('click')
+
+    expect(selectedValue).toBe('IT')
+  })
+})
