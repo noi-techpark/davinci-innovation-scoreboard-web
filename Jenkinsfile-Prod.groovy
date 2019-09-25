68c3c05891eb589d6df4d6d052a3bb3770af8a87
Alex Lanz <alex@krumer.it>
2019-09-25 14:59:09 +0200

    Fix wrong API endpoint

--- Jenkinsfile-Prod.groovy
+++ Jenkinsfile-Prod.groovy
@@ -10,7 +10,7 @@ pipeline {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
 
-        API = "https://api.innovation.davinci.bz.it"
+        API = "https://api.innovation.davinci.bz.it/v1/"
     }
 
     stages {
